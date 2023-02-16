5a29497b9a4d11137250f05585e3af48302ca4a9
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-17 00:05:24 +0600

    math problem solved

--- 04-Generate RGB Color By Clicking/js/apps.js
+++ 04-Generate RGB Color By Clicking/js/apps.js
@@ -19,10 +19,12 @@ function main(){
  
 
 // step 2 - random color generator function
-function generateRGBColor() {
-    const red = Math.floor(Math.random()* 255);
-    const green = Math.floor(Math.random()* 255);
-    const blue = Math.floor(Math.random()* 255);
 
-    return  'rgb(${red}, ${green}, ${blue})';
+function generateRGBColor() {
+
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return  `rgb(${red}, ${green}, ${blue})`;
 }
