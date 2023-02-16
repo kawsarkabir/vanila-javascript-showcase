b42f662c63c6981a661b605c48137fe9ff9461db
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-16 12:56:40 +0600

    add new product items done

--- 03-calculate product/js/script.js
+++ 03-calculate product/js/script.js
@@ -3,32 +3,44 @@ var allProducts = []
 
 function displayProduct(){
     var tableBody = document.getElementById('table_body')
-    for(var i = 0; i< allProducts.length; i++){
-        var newProductRow = document.createElement('tr')
+    tableBody.innerHTML = null
+    for(var i =0; i<allProducts.length; i++){
+        var newProductRow=document.createElement('tr')
         var colum_1 = document.createElement('td')
         var colum_2 = document.createElement('td')
         var colum_3 = document.createElement('td')
 
-        colum_1.innerText = i+1
-        colum_2.innerText = allProducts[i].name
-        colum_2.innerText = allProducts[i].price
-        newProductRow.append(colum_1, colum_2,colum_3)
+        colum_1.innerText=i+1
+        colum_2.innerText=allProducts[i].name
+        colum_3.innerText=allProducts[i].price
+        newProductRow.append(colum_1, colum_2, colum_3)
         tableBody.append(newProductRow)
+         
     }
- }
+}
+
+
+
+
+
+
+
 
 
 
 formSubmit.addEventListener('submit', function(event){
     event.preventDefault();
+
     var productName = document.getElementById('product_name').value;
     var productPrice = document.getElementById('product_price').value;
-
-    var product = {
-        name:productName,
+    var productPriceHide = document.getElementById('product_name');
+    var productNameHide = document.getElementById('product_price');
+    var product={
+        name: productName,
         price:productPrice
     }
-    allProducts = [allProducts.length] = product;
-    displayProduct();
-
+    allProducts[allProducts.length]=product;
+    productPriceHide.value=""
+    productNameHide.value=""
+    displayProduct()
 })
