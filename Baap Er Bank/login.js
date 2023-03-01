a67e39007897b86dba95a116867129592059ee35
kawsarkabir <kawsarkabir11@gmail.com>
2023-03-01 18:20:55 +0600

    loging succesfully

--- Baap Er Bank/login.js
+++ Baap Er Bank/login.js
@@ -1,3 +1,14 @@
-document.getElementById('deposit-btn').addEventListener('click' , function(){
-    console.log('gk');
+document.getElementById('login-btn').addEventListener('click' , function(){
+    const inputField = document.getElementById('input-field')
+    const email = inputField.value;
+    
+    const passwordField = document.getElementById('password-field')
+    const password = passwordField.value;
+
+    if(email == 'k2@gmail.com' && password == 'kawsark2'){
+        console.log('asol malik');
+    }
+    else{
+        console.log('hack by unknow person');
+    }
 })
