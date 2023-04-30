99df41cfecf89b51f6132b882dda4a4dad161bc8
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-30 11:56:08 +0600

    case card calculate done

--- 07-Shooping Card/js/case.js
+++ 07-Shooping Card/js/case.js
@@ -1,1 +1,27 @@
- 
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberFieldString = caseNumberField.value;
+    const previousCaseNumebr = parseInt(caseNumberFieldString);
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumebr + 1;
+    }else{
+        newCaseNumber = previousCaseNumebr - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click' , function(){
+    const newCaseNumber = updateCaseNumber(true)
+    updateCaseTotalPrice(newCaseNumber);
+})
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+})
