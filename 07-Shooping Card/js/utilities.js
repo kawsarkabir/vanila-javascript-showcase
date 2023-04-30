05e93f8cd62b5a6375b58399671324da1f59c47f
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-30 12:56:34 +0600

    shopping card project done

--- 07-Shooping Card/js/utilities.js
+++ 07-Shooping Card/js/utilities.js
@@ -4,10 +4,25 @@ function getTextElementById(elementId){
     const currentTotal = parseInt(getElementsString);
     return currentTotal;
 }
+function setTextElementValuById(element, value){
+    const totalElement = document.getElementById(element)
+    totalElement.innerText = value;
+}
 function getSubTotal(){
     const currentPhoneTotal = getTextElementById('phone-total');
     const currentCaseTotal = getTextElementById('case-total');
+    
     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
+    setTextElementValuById('sub-total', currentSubTotal);
+
+    // calculate tax
+    const texAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const texAmount = parseFloat(texAmountString);
+    setTextElementValuById('tax-amount', texAmount);
+    // calclulator final amount
+    const finalAmount = currentSubTotal + texAmount;
+    setTextElementValuById('final-total', finalAmount);
 }
+document.getElementById('check-out').addEventListener('click', function(){
+    alert('Please Check Out !')
+})
