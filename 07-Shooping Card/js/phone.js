3bc6d34637f0ce5ccbcf34a244dc867c2ad8b084
kawsarkabir <devkawsarkabir@gmail.com>
2023-04-30 11:40:13 +0600

    phone card calculate done

---
+++ 07-Shooping Card/js/phone.js
@@ -0,0 +1,27 @@
+function updatePhoneNumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberFieldString = phoneNumberField.value;
+    const previousPhoneNumberField = parseInt(phoneNumberFieldString);
+
+    let newPhoneNumber;
+    if(isIncrease === true){
+        newPhoneNumber = previousPhoneNumberField + 1;
+    }else{
+        newPhoneNumber = previousPhoneNumberField - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+document.getElementById('btn-phone-plus').addEventListener('click' , function(){
+    const newPhoneNumber = updatePhoneNumber(true);  
+    updatePhoneTotalPrice(newPhoneNumber);
+})
+document.getElementById('btn-phone-minus').addEventListener('click', function(){
+    const newPhoneNumber = updatePhoneNumber(false);
+    updatePhoneTotalPrice(newPhoneNumber);
+})
