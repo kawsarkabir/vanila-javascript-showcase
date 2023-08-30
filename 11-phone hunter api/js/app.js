55b19a393f7eb862ba0ba0cb38eea6e86fdb14c2
kawsarkabir <devkawsarkabir@gmail.com>
2023-08-30 18:16:14 +0600

    show all btn click then all item show

--- 11-phone hunter api/js/app.js
+++ 11-phone hunter api/js/app.js
@@ -1,55 +1,76 @@
-const loadPhones = async (searchText) =>{
+const loadPhones = async (searchText='iphone', isShowAll) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    showPhones(phones);
+    showPhones(phones, isShowAll);
 }
 
-const showPhones = phones =>{
+const showPhones = (phones, isShowAll) =>{
     const phoneCardContainer = document.getElementById('phone-container');
     phoneCardContainer.innerText = ''
 
     // show all phone button
-    const showAllContainer = document.getElementById('showAll-btn');
-    if(phones.length > 12){
+    const showAllContainer = document.getElementById('showAll-container');
+    if(phones.length > 12 && !isShowAll){
         showAllContainer.classList.remove('hidden');
     }else{
         showAllContainer.classList.add('hidden')
     }
 
+    // show only frist 12 phone if not show all
+    if(!isShowAll){
+        phones = phones.slice(0, 9)
+    }
+
     // phone not found
-    // const notFoundMassage = document.getElementById('not-found-massage');
-    // notFoundMassage.classList.add('warning-phones')
-    // if(phones.length === 0){
-    //     notFoundMassage.classList.remove('display-none')
-    // }else{
-    //     notFoundMassage.classList.add('display-none', true)
-    // }
+    const notFoundMassage = document.getElementById('not-found-massage');
+    if(phones.length === 0){
+        notFoundMassage.classList.remove('hidden')
+    }else{
+        notFoundMassage.classList.add('hidden')
+    }
 
-    // show only frist 12 phone
-    phones = phones.slice(0, 9)
 
-    // display all phone
+    // display all phone lopping
     phones.forEach(phone =>{
-         const phoneCard = document.createElement('div');
-        phoneCard.classList.add('card');
+        const phoneCard = document.createElement('div');
+        phoneCard.classList.add('phone-card');
         phoneCard.innerHTML = `
         <img src=${phone.image} alt="">
         <h3>${phone.phone_name}</h3>
         <p>There are many variations of passages of available, but the majority have suffered</p>
         <h4>$999</h4>
-        <button class="sign-up">Show Details</button>
+        <button onclick="handleShowDetail('${phone.slug}')" class="sign-up">Show Details</button>
         `
         phoneCardContainer.appendChild(phoneCard)
-    })
+    });
+}
+
+// handleShowDetail info button
+const handleShowDetail = async (id)=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    displayShowDetails(phone)
+}
+
+// displayShowDetails individual items
+const displayShowDetails = (phone) =>{
+    console.log(phone);
 }
 
 // handle search btn
-const handleSearchBtn =()=>{
+const handleSearchBtn =(isShowAll)=>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
-    searchField.value = ''
+    loadPhones(searchText, isShowAll);
+    // searchField.value = ''
 }
+
+// handle show all
+const handleShowAll = ()=>{
+    handleSearchBtn(true);
+}
+
 
 loadPhones()
